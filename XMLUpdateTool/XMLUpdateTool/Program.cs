e03b0f24cbf81ab135b5b052170b7648f7393871
Aniket Biprojit Chowdhury <aniket.chowdhury@peppercontent.io>
2022-07-27 08:34:07 +0530

    feat: init XML tool

---
+++ XMLUpdateTool/XMLUpdateTool/Program.cs
@@ -0,0 +1,19 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+string baseDir = "/Users/peppercontent/Work/openxml-tool/data/";
+
+string filepath = baseDir + "test-doc.docx";
+
+//int x = 20;
+//int y = 60;
+
+using (WordprocessingDocument document =
+       WordprocessingDocument.Open(filepath, true))
+{
+    if (document.MainDocumentPart != null) {
+        MainDocumentPart mainDocumentPart = document.MainDocumentPart;
+        Console.WriteLine("File read.");
+        Console.WriteLine(mainDocumentPart.Document.Descendants().Count());
+    }
+}
