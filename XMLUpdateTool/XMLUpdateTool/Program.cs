ee3d7cea80c9dd9107e6d5b7827e0c8447a54128
Aniket Biprojit Chowdhury <aniket.chowdhury@peppercontent.io>
2022-07-27 09:48:48 +0530

    fix: pudate content

--- XMLUpdateTool/XMLUpdateTool/Program.cs
+++ XMLUpdateTool/XMLUpdateTool/Program.cs
@@ -22,6 +22,7 @@ Console.WriteLine(textToSearch);
 Run? forX = null;
 Run? forY = null;
 
+
 using (WordprocessingDocument document =
        WordprocessingDocument.Open(filepath, true))
 {
@@ -43,24 +44,71 @@ using (WordprocessingDocument document =
                 Console.WriteLine("y = " + y + " " + textFromDoc[y]);
             }
         }
-    }
-    if(forX!=null && forY != null)
-    {
-        if (textFromDoc[x..y] == textToSearch) {
-            Console.WriteLine("Matched");
+
+        if (forX != null && forY != null)
+        {
+            if (textFromDoc[x..y] == textToSearch)
+            {
+                Console.WriteLine("Matched");
+                Comments comments = null;
+
+                string initialCommentId = "0";
+
+                if (mainDocumentPart.GetPartsCountOfType<WordprocessingCommentsPart>() > 0)
+                {
+                    comments = mainDocumentPart.WordprocessingCommentsPart.Comments;
+                    if (comments.HasChildren)
+                    {
+                        // Obtain an unused ID.
+                        initialCommentId = (comments.Descendants<Comment>().Select(e => int.Parse(e.Id.Value)).Max() + 1).ToString();
+                    }
+                }
+                else
+                {
+                    // No WordprocessingCommentsPart part exists, so add one to the package.
+                    WordprocessingCommentsPart commentPart = mainDocumentPart.AddNewPart<WordprocessingCommentsPart>();
+                    commentPart.Comments = new Comments();
+                    comments = commentPart.Comments;
+                }
+
+
+                Paragraph p = new Paragraph(new Run(new Text("Added Comment 1")));
+                Comment cmt =
+                    new Comment()
+                    {
+                        Id = initialCommentId,
+                        Author = "Aniket Biprojit Chowdhury",
+                        Initials = "AC",
+                        Date = DateTime.Now
+                    };
+                cmt.AppendChild(p);
+                comments.AppendChild(cmt);
+                comments.Save();
+
+                // add a commentRangeStart before forX
+                forX.InsertBeforeSelf(new CommentRangeStart(){ Id = initialCommentId });
+
+                // add a commentRangeEnd after forY
+                var cmtEnd = forY.InsertAfterSelf(new CommentRangeEnd() { Id = initialCommentId });
+
+
+                // add a commentReference after commentRangeEnd
+                cmtEnd.InsertAfterSelf(new Run(new CommentReference() { Id = initialCommentId }));
+
+                document.Save();
+            }
+            else
+            {
+                Console.WriteLine(textFromDoc[x..y]);
+                Console.WriteLine(textToSearch);
+                Console.WriteLine(textFromDoc);
+                throw new Exception("text does not match");
+            }
         }
         else
         {
-            Console.WriteLine(textFromDoc[x..y]);
-            Console.WriteLine(textToSearch);
-            Console.WriteLine(textFromDoc);
-            throw new Exception("text does not match");
+            throw new Exception("forX or forY null");
         }
-    }
-    else
-    {
-        throw new Exception("forX or forY null");
-    }
 
-    
+    }
 }
